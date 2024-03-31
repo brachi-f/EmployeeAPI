d69cb2e070c080cbe18bd394a31d2277fa31e3dd
brachi-f <braf5866@gmail.com>
2024-03-31 21:31:19 +0300

    cors

--- Employees.Api/Program.cs
+++ Employees.Api/Program.cs
@@ -34,6 +34,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(builder =>
+        builder
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+
 app.UseAuthorization();
 
 app.MapControllers();
